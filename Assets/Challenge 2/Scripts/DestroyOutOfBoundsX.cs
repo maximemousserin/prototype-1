b1117e5988a9d404fa2462fe89dfb446768c2c17
maximemousserin <maxime.mousserin@gmail.com>
2024-04-12 10:00:27 +0200

    Challenge 2

---
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Challenge2
+{
+    public class DestroyOutOfBoundsX : MonoBehaviour
+    {
+        private float leftLimit = -30;
+        private float bottomLimit = -6;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Destroy dogs if x position less than left limit
+            if (transform.position.x < leftLimit)
+            {
+                Destroy(gameObject);
+            }
+            // Destroy balls if y position is less than bottomLimit
+            else if (transform.position.y < bottomLimit)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Challenge2
+{
+
+    public class SpawnManagerX : MonoBehaviour
+    {
+        public GameObject[] ballPrefabs;
+
+        private float spawnLimitXLeft = -22;
+        private float spawnLimitXRight = 7;
+        private float spawnPosY = 30;
+
+        private float startDelay = 1.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            float spawnInterval = Random.Range(3, 5);
+            InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        }
+
+        // Spawn random ball at random x position at top of play area
+        void SpawnRandomBall()
+        {
+            Debug.Log("SpawnRandomBall");
+
+            // Generate random ball index and random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+            // Random ballPrefabs index
+            int ballPrefabIndex = Random.Range(0, ballPrefabs.Length - 1);
+
+            Debug.Log("SpawnRandomBall " + ballPrefabIndex);
+
+            var ballPrefab = ballPrefabs[ballPrefabIndex];
+
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefab, spawnPos, ballPrefab.transform.rotation);
+        }
+
+    }
+
+}
